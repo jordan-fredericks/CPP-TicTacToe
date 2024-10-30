6582b7b695fcd44aeec2fe7c84c3872c6ec34e6e
Jordan Fredericks <500172971@fvtc.edu>
2024-10-30 18:29:05 -0500

    Moved the methods into the TicTacToe cpp file

---
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -0,0 +1,108 @@
+#include "TicTacToe.h"
+
+bool TicTacToe::IsGameOver() const
+{
+	//Check all 8 victory conditions
+	if (m_board[0] == m_board[1] && m_board[1] == m_board[2])
+	{
+		Victor(m_board[0]);
+		return true;
+	}
+	else if (m_board[3] == m_board[4] && m_board[4] == m_board[5])
+	{
+		Victor(m_board[3]);
+		return true;
+	}
+	else if (m_board[6] == m_board[7] && m_board[7] == m_board[8])
+	{
+		Victor(m_board[6]);
+		return true;
+	}
+	else if (m_board[0] == m_board[3] && m_board[3] == m_board[6])
+	{
+		Victor(m_board[0]);
+		return true;
+	}
+	else if (m_board[1] == m_board[4] && m_board[4] == m_board[7])
+	{
+		Victor(m_board[1]);
+		return true;
+	}
+	else if (m_board[2] == m_board[5] && m_board[5] == m_board[8])
+	{
+		Victor(m_board[2]);
+		return true;
+	}
+	else if (m_board[0] == m_board[4] && m_board[4] == m_board[8])
+	{
+		Victor(m_board[0]);
+		return true;
+	}
+	else if (m_board[2] == m_board[4] && m_board[4] == m_board[6])
+	{
+		Victor(m_board[2]);
+		return true;
+	}
+	//Check if all 9 spots on the board are taken if no victory condition is found
+	else if (m_turn > 9)
+	{
+		std::cout << "It's a tie\n";
+		return true;
+	}
+	//Return false if no victory or tie is found
+	else { return false; }
+}
+
+//This function is to see which player won when a victory is found
+void TicTacToe::Victor(char marker) const
+{
+	if (marker == 'X') { std::cout << "Player 1 wins!\n"; }
+	else { std::cout << "Player 2 wins!\n"; }
+}
+
+//Display the current state of the board
+void TicTacToe::Display() const
+{
+	system("cls");
+	std::cout << m_board[0] << "|" << m_board[1] << "|" << m_board[2] << "\n";
+	std::cout << "-----\n";
+	std::cout << m_board[3] << "|" << m_board[4] << "|" << m_board[5] << "\n";
+	std::cout << "-----\n";
+	std::cout << m_board[6] << "|" << m_board[7] << "|" << m_board[8] << "\n";
+}
+
+//The current player places their marker on the board in an unnocupied space, then the active player changes and the turn counter goes up
+void TicTacToe::TakeTurn()
+{
+	int choice = 0;
+	char marker = 'Q';
+
+	std::cout << "Player " << m_player << "'s turn.\n";
+
+	//Assigns player 1 the X and player 2 the O
+	if (m_player == 1) { marker = 'X'; }
+	else { marker = 'O'; }
+
+	while (choice < 1 || choice > 9)
+	{
+		std::cout << "Where would you like to place your piece: ";
+		std::cin >> choice;
+		//Checks if that spot is on the board
+		if (choice < 1 || choice > 9) { std::cout << "Please place your piece on the board.\n"; }
+		//Checks if that spot is already taken
+		else if (m_board[choice - 1] == 'X' || m_board[choice - 1] == 'O')
+		{
+			std::cout << "Please place your piece on an empty spot.\n";
+			choice = 0;
+		}
+		//If the spot chosen is on the board and not already taken, place the marker down
+		else { m_board[choice - 1] = marker; }
+	}
+
+	//Change the active player at the end of the turn
+	if (m_player == 1) { m_player = 2; }
+	else { m_player = 1; }
+
+	//Increment the turn counter
+	m_turn += 1;
+}
